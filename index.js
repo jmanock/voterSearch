16fc8574c15be1fe6089b1d6af07e001176cbf5f
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2017-11-07 17:40:27 -0500

    starting to understand this

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const id = '121J0T9n5xpGbaBMDsgvdimYBfzNr31mBH20EaI5zJmQ';
 const drive = require('drive-db')(id);
 const homeRoute = get('/', async () =>{
   const db = await drive.load();
-  console.log(db.find());
+  console.log('HelloFriend: ',db.find());
   return render('index.hbs', {users:db.find()});
+});
+
+const prom = drive.update(id, 'db.json').then(db =>{
+  console.log('updated new copy')
+}).catch(err => {
+  console.log('There was an error');
 });
 server(3000, homeRoute);
