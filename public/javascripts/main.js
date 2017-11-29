24a7dc9912b89549a85a2a50e62604c73aa82fd7
Jon Manock <jonmanock@Jons-MacBook-Air.local>
2017-11-29 12:54:58 -0500

    now to show the data that was returned

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -17,13 +17,22 @@ $(document).ready(function(){
         if(data instanceof Array){
           for(var i = 0; i<data.length; i++){
             var add2 = data[i].Address2;
+            var fName = data[i].FirstName;
+            var lName = data[i].LastName;
+            var fullName = fName + ' '+ lName;
+            var address = data[i].Address;
+            var city = data[i].City;
+            var age = data[i].Age;
+            var birthday = data[i].Dob;
+            var city = data[i].City;
+            var gender = data[i].Gender;
+            var zip = data[i].Zip;
             if(add2 !== undefined){
-              // console.log('Yess');
-              console.log(data[i]);
+              console.log(fullName, address, add2, city, age, birthday, gender, zip);
             }else{
-              // console.log('No!');
+              console.log(fullName, address, city, zip, age, birthday,gender);
             }
-            var fName = data[i].fName;
+
           }
         }
       });
